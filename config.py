e23ce81e044de165f2a8d63ec074977382a3c13d
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-07-02 22:32:31 -0700

    initial files

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 # Load from environment variables or hard-code for testing
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "your-api-key-here")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 DEFAULT_MODEL = "gpt-4o"  # Can fallback to "gpt-3.5-turbo"
+
+
+
+
+# === Environment ===
+
+#os.environ['OPENAI_API_KEY'] = 
+#os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
